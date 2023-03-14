bf367cf1689ec1c4e5172199b4c2c5aa1b165e9b
Vaibhav Thorat <vaibhavthorat141997@gmail.com>
2023-03-14 16:37:55 +0530

    added color to letters when hovered over

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,23 @@
 import React from "react";
 
 const About = () => {
+  
+  const text =
+    "Hi, I'm Vaibhav, a web developer with a year of experience. I started with The Odin Project and later enrolled in Almabetter's Fullstack bootcamp to build projects and improve my skills. Despite CSS being commonly disliked, it's my favorite. In 2020, I founded my own production house where I gained experience in SEO, content management, team building, and team management.";
   return (
     <div className="flex-col flex items-center justify-center mt-12 text-[#F6E8EA]">
       <div className="flex items-center">
-        <div className="text-4xl my-12 w-8 ml-[14vw] mr-[6vw]">ABOUT ME</div>
-        <div className="w-[40vw] text-justify font-mono">
-          Hi, I'm Vaibhav, a web developer with a year of experience. I started
-          with The Odin Project and later enrolled in Almabetter's Fullstack
-          bootcamp to build projects and improve my skills. Despite CSS being
-          commonly disliked, it's my favorite. In 2020, I founded my own
-          production house where I gained experience in SEO, content management,
-          team building, and team management.
+        <div className="text-4xl my-12 w-8 ml-[34vw]">ABOUT ME</div>
+        <div className="container flex justify-center items-center ">
+          <div className="text-container w-3/4 rounded-lg">
+            <p className="subtitle text-lg font-mono text-justify w-8/12">
+              {text.split("").map((char, index) => (
+                <span key={index} className="hover:text-green-500">
+                  {char}
+                </span>
+              ))}
+            </p>
+          </div>
         </div>
       </div>
       <div className="text-2xl font-bold mt-[12vh] mb-8">
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,34 @@
 import React from "react";
 import { AiOutlineGithub, AiFillLinkedin } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className="">
-      <AiOutlineGithub />
-      <AiFillLinkedin />
+    <div className="fixed justify-center bottom-2 left-2 flex flex-col text-[#F6E8EA] text-3xl gap-4">
+      <Link
+        to="https://github.com/Vaithorat"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <AiOutlineGithub className="opacity-30 hover:opacity-100" />
+      </Link>
+      <Link
+        to="https://www.linkedin.com/in/vaibhav-thorat/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <AiFillLinkedin className="opacity-30 hover:opacity-100" />
+      </Link>
+      <div
+        className="ml-3 opacity-30 "
+        style={{
+          writingMode: "vertical-lr",
+          borderLeft: "2px solid [#F6E8EA]  ",
+          paddingLeft: "10px",
+          height: "50px",
+          opacity: "0.5",
+        }}
+      ></div>
     </div>
   );
 };
