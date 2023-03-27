c5f1ebc2bff0dede2b99856edec64158a4e924c4
Vaibhav Thorat <vaibhavthorat141997@gmail.com>
2023-03-27 12:51:33 +0530

    fixed deps

--- src/components/Work.js
+++ src/components/Work.js
@@ -8,10 +8,10 @@ const Work = () => {
     <div
       id="work"
       name="work"
-      className="w-full md:h-screen text-gray-300 bg-[#0a192f]"
+      className="w-full md:h-screen text-gray-300 bg-[#0a192f] md:mt-8"
     >
       <div className="max-w-[1200px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
+        <div className="py-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-[#3D9239] ">
             Projects that I've worked on
           </p>
