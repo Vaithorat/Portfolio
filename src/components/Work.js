c94329c05b38a61003353cec6ca6349f94bbad04
Vaibhav Thorat <vaibhavthorat141997@gmail.com>
2023-05-31 11:43:31 +0530

    updated projects

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import { Carousel } from "react-responsive-carousel";
 import { data } from "../data/data.js";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Work = () => {
   const project = data;
@@ -16,45 +18,56 @@ const Work = () => {
   return (
     <div
       id="work"
-      name="work"
-      className="w-full md:h-screen text-gray-300 bg-[#0a192f] md:mt-8"
+      className="w-full md:h-screen bg-[#0a192f] md:mt-8 text-gray-300"
     >
-      <div className="max-w-[1200px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+      <div className="max-w-[1200px] mx-auto p-4 h-full flex flex-col justify-center">
         <div className="py-8">
-          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-[#3D9239] ">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-[#3D9239]">
             Projects that I've worked on
           </p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-y-28 ">
+        <Carousel
+          showThumbs={false}
+          autoPlay={true}
+          infiniteLoop={true}
+          showArrows={false}
+          showStatus={false}
+          stopOnHover={true}
+        >
           {project.map((item, index) => (
             <div
               key={index}
-              style={{ backgroundImage: `url(${item.image})` }}
-              className="shadow-lg group container rounded-md 
-              flex justify-center text-center items-center mx-auto content-div relative"
+              className="container pb-12 mx-auto flex justify-center items-center relative content-div shadow-lg rounded-md group"
               onMouseEnter={() => handleMouseEnter(index)}
               onMouseLeave={handleMouseLeave}
             >
               <div className={`intro${showIntro === index ? " visible" : ""}`}>
-                <div className="absolute  w-full h-20 translate-y-full bg-black opacity-80 flex items-center justify-center">
-                  <div className="text-white text-center mx-4 ">
+                <div className="absolute w-full h-20 translate-y-full bg-black opacity-80 flex items-center justify-center">
+                  <div className="text-white text-center mx-4">
                     {item.intro}
                   </div>
                 </div>
               </div>
-              <div className="opacity-0 group-hover:opacity-100 ">
-                <span className="text-2xl font bold text-white tracking-wider ">
+              <div className="opacity-100">
+                <div className="h-80 md:h-96 pb-8">
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    className="object-cover w-full  rounded-xl h-full"
+                  />
+                </div>
+                <span className="text-2xl font-bold text-white tracking-wider">
                   {item.name}
                 </span>
-                <div className="pt-8 text-center ">
+                <div className="pt-8 text-center">
                   <a href={item.github} target="_blank" rel="noreferrer">
-                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    <button className="px-4 py-3 m-2 text-lg font-bold text-gray-700 bg-white rounded-lg">
                       Code
                     </button>
                   </a>
                   <a href={item.live} target="_blank" rel="noreferrer">
-                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    <button className="px-4 py-3 m-2 text-lg font-bold text-gray-700 bg-white rounded-lg">
                       Live
                     </button>
                   </a>
@@ -62,7 +75,7 @@ const Work = () => {
               </div>
             </div>
           ))}
-        </div>
+        </Carousel>
       </div>
     </div>
   );
--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,7 @@
 import crypto from "../assets/projects/crypto.jfif";
 import ai from "../assets/projects/ai.jfif";
 import social from "../assets/projects/social.jfif";
+import ecomm from "../assets/projects/ecomm.jpg";
 
 export const data = [
   {
@@ -9,7 +10,8 @@ export const data = [
     image: crypto,
     github: "https://github.com/Vaithorat/CryptoCurrency-Dashboard",
     live: "https://cryptodashboard-alma.netlify.com/",
-    intro: "Live cryptocurrency price tracking app made using React, Redux, Tailwind and Coingecko API"
+    intro:
+      "Live cryptocurrency price tracking app made using React, Redux, Tailwind and Coingecko API",
   },
   {
     id: 2,
@@ -17,7 +19,8 @@ export const data = [
     image: ai,
     github: "https://github.com/Vaithorat/AiImageGenerator",
     live: "https://dalleimggen.netlify.app/",
-    intro: "AI image generator using user prompts. It uses Vite-React, Dall-e API, Tailwind and Cloudinary "
+    intro:
+      "AI image generator using user prompts. It uses Vite-React, Dall-e API, Tailwind and Cloudinary ",
   },
   {
     id: 3,
@@ -25,6 +28,16 @@ export const data = [
     image: social,
     github: "https://github.com/Vaithorat/SocialMediaApp",
     live: "https://retrogramsocial.netlify.app/",
-    intro: "Create profile, add friends and more on Retrogram. It uses React, Redux, Material UI, MongoDB and JWT"
+    intro:
+      "Create profile, add friends and more on Retrogram. It uses React, Redux, Material UI, MongoDB and JWT",
+  },
+  {
+    id: 4,
+    name: "E-commerce Website",
+    image: ecomm,
+    github: "https://github.com/Vaithorat/bondsindia",
+    live: "https://ecommdemoproject.netlify.app/home",
+    intro:
+      "E-commerce website built using React, Redux-toolkit. Authentication is done using Firebase",
   },
 ];
